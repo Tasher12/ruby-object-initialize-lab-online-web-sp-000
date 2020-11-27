8c7119b51e0f46a9735ccca8db4b4cefc52a3350
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-11-27 14:13:53 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog 
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name = name
+  end
+  
+  def name
+    @name = name
+  end
+  
+  def breed(breed = "Mutt")
+    @breed = breed
+  end
+  
+  def breed 
+    @breed 
+  end
+end
